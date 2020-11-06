69bed8771796b78e27854b4fbcd35d0e65c38eb9
Luiza Campos <luiza.almcampos@gmail.com>
2020-11-06 09:32:06 -0300

    done

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Margherita', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'A tradicional italiana!'},
+    {id:2, name:'Quatro Queijos', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Mussarela, provolone, requeijão e gorgonzola'},
+    {id:3, name:'Portuguesa', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Tomates, azeitonas, pimentões e o toque do chef: cogumelos!'},
+    {id:4, name:'Brócolois com requeijão', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Brócolis, requeijão, tomatinhos e azeitonas'},
+    {id:5, name:'Americana', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Pimentões, azeitonas, milho e brócolis juntos, no melhor estilo americano!'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Nosso sabor especial, elaborado especialmente pelo nosso chef'},
+    {id:7, name:"M&M's", img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:"Chocolate ao leite coberto com deliciosos M&M's"}
+];
